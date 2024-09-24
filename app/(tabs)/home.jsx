cf34602339068e5405aa800a6fc9d299ac2546ac
Mac <mac@Macs-MacBook-Pro.local>
2024-09-24 21:02:16 +0100

    trending, searchinput, emptystate

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,14 +1,25 @@
-import { View, Text, FlatList, Image, } from 'react-native'
+import { View, Text, FlatList, Image, RefreshControl, } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { images } from '../../constants'
+import SearchInput from '../../components/SearchInput'
 
-import React from 'react'
+import React, { useState } from 'react'
+import Trending from '../../components/Trending'
+import EmptyState from '../../components/EmptyState'
 
 const Home = () => {
+const [refreshing, setRefreshing] = useState(false);
+ 
+const onRefresh = async() => {
+  setRefreshing(true)
+  // re call videos- see if any new video appears
+}
+
   return (
    <SafeAreaView className="bg-primary h-full" >
    <FlatList
     data={[{id: 1}, {id: 2}, {id: 1}]}
+    // data={{}}
     keyExtractor={(item) => item.$id}
     renderItem={({item}) => (
       <Text className="text-3xl text-white" >{item.id}</Text>
@@ -32,11 +43,26 @@ const Home = () => {
             />
           </View>
         </View>
-        
+        <SearchInput/>
+        <View className="w-full flex-1 pt-5 pb-8">
+            <Text className="text-gray-100 text-lg font-pregular mb-3">
+              Latest Videos
+            </Text>
+
+            <Trending posts ={[{id: 1}, {id: 2}, {id:3}] ?? [] }/>
+        </View>
       </View>
      )}
+     ListEmptyComponent={() => (
+       <EmptyState 
+       title="No Videos Found"
+       subtitle="Be the first one to upload a video"
+       />
+     )}
+     refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh}/>}
    />
    </SafeAreaView>
+
   )
 }
 
--- components/SearchInput.jsx
+++ components/SearchInput.jsx
@@ -1,14 +1,34 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { View, Text, TextInput, TouchableOpacity, Image } from 'react-native'
+import React, {useState} from 'react'
 
-const SearchInput = () => {
+import { icons } from '../constants'
+import images from '../constants'
+
+const SearchInput = ({title, value, placeholder, handleChangeText, otherStyles, ...props}) => {
+
+  const [showPassword, setShowPassword] = useState(false)
   return (
-    <View>
-      <Text>SearchInput</Text>
-    </View>
+    
+
+      <View className="border-2 border-black-100 w-full h-16 px-4 bg-black-100 rounded-2xl focus:border-secondary items-center flex-row space-x-4">
+        <TextInput
+         className=" text-base mt-0.5 text-white flex-1 font-pregular"
+         value={value}
+         placeholder="Search for a video topic"
+         placeholderTextColor='#7b7b8b'
+         onChangeText={handleChangeText}
+         secureTextEntry={title=== 'Password' && !showPassword}
+         />
+        <TouchableOpacity>
+            <Image
+            source={icons.search}
+            className="w-5 h-5"
+            resizeMode='contain'
+            />
+        </TouchableOpacity>
+      </View>
+   
   )
 }
 
 export default SearchInput
-
-const styles = StyleSheet.create({})
