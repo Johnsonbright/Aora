a5610cf7465f3c3719b4c54ebfad0c0dca37bd2d
Mac <mac@Macs-MacBook-Pro.local>
2024-09-26 15:22:26 +0100

    videocard added

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -8,6 +8,7 @@ import Trending from '../../components/Trending'
 import EmptyState from '../../components/EmptyState'
 import {getAllPosts} from "../../lib/appwrite"
 import useAppwrite from '../../lib/useAppwrite'
+import VideoCard from '../../components/VideoCard'
 
 const Home = () => {
 
@@ -21,14 +22,17 @@ const onRefresh = async() => {
   // re call videos- see if any new video appears
   setRefreshing(false);
 }
-console.log(posts)
+
   return (
    <SafeAreaView className="bg-primary h-full" >
    <FlatList
     data={posts}
     keyExtractor={(item) => item.$id}
     renderItem={({item}) => (
-      <Text className="text-3xl text-white" >{item.title}</Text>
+      <VideoCard 
+      video={item}
+      
+      />
     )}
      ListHeaderComponent={()=> (
       <View className="my-6 px-4 space-y-6"  >
