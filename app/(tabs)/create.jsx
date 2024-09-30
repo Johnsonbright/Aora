7fedd3e58fe34e36305c670beaac8d533ae4da5e
Mac <mac@Macs-MacBook-Pro.local>
2024-09-30 16:18:50 +0100

    setting the video and thumbnail

--- app/(tabs)/create.jsx
+++ app/(tabs)/create.jsx
@@ -1,7 +1,10 @@
-import { View, Text, ScrollView } from 'react-native'
+import { View, Text, ScrollView, TouchableOpacity, Image } from 'react-native'
 import React, {useState} from 'react'
 import {SafeAreaView} from "react-native-safe-area-context"
 import FormField from "../../components/FormField"
+import {Video , ResizeMode} from "expo-av"
+import { icons } from '../../constants'
+import CustomButton from "../../components/CustomButton"
 
 const create = () => {
 
@@ -12,6 +15,14 @@ const [uploading, setUploading] = useState(false)
   thumbnail: null,
   prompt: " "
  })
+
+ const openPicker = async(selectType) => {
+    const result = await DocumentPicker
+ };
+
+ const submit = () => {
+
+ };
 
   return (
    <SafeAreaView className="bg-primary h-full">
@@ -31,7 +42,73 @@ const [uploading, setUploading] = useState(false)
       <Text className="text-base text-gray-100 font-pmedium">
         Upload Video
       </Text>
+
+      <TouchableOpacity onPress ={() => openPicker('video')}>
+        {form.video ? 
+        <Video
+         source={{uri: form.video.uri}}
+         className="w-full h-64 rounded-2xl"
+         useNativeControls
+         resizeMode={ResizeMode.COVER}
+         isLooping
+        />  
+        : <View className="w-full h-40 px-4 bg-black-100 rounded-2xl justify-center items-center">
+          <View className="w-14 h-14 border border-dashed border-secondary-100 justify-center items-center">
+          <Image
+           source={icons.upload}
+           resizeMode='contain'
+           className="w-1/2 h-1/2"
+           />
+          </View>
+
+        </View>
+      }
+      </TouchableOpacity>
      </View>
+
+     <View className="mt-7 space-y-2">
+     <Text className="text-base text-gray-100 font-pmedium">
+      Thumbnail Image
+      </Text>
+
+      <TouchableOpacity onPress ={() => openPicker('image')}>
+        {form.thumbnail ? 
+        <Image 
+        source={{uri: form.thumbnail.url}}
+        resizeMode='cover'
+        className="w-full h-64 rounded-2xl"
+        />
+       
+        : <View className="w-full h-16 px-4 bg-black-100 rounded-2xl justify-center items-center border-2 border-black-200 flex-row space-x-2">
+          <Image
+           source={icons.upload}
+           resizeMode='contain'
+           className="w-5 h-5"
+           />
+           <Text className="text-sm text-gray-100 font-pmedium" >
+            Choose a file
+           </Text>
+          </View>
+
+     
+      }
+      </TouchableOpacity>
+     </View>
+
+     <FormField
+      title="AI Prompt"
+      value={form.prompt}
+      placeholder="The prompt you used to create this video"
+      handleChangeText={(e) => setForm({...form, prompt: e})}
+      otherStyles="mt-7"
+     />
+
+     <CustomButton
+      title="Submit & Publish"
+      handlePress={submit}
+      containerStyles="mt-7"
+      isLoading={uploading}
+     />
     </ScrollView>
    </SafeAreaView>
   )
