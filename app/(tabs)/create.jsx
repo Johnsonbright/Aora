2dcb57ce643ea97cd500924d7b3de6949a1ab7e2
Mac <mac@Macs-MacBook-Pro.local>
2024-09-30 14:06:34 +0100

    create

--- app/(tabs)/create.jsx
+++ app/(tabs)/create.jsx
@@ -1,11 +1,39 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView } from 'react-native'
+import React, {useState} from 'react'
+import {SafeAreaView} from "react-native-safe-area-context"
+import FormField from "../../components/FormField"
 
 const create = () => {
+
+const [uploading, setUploading] = useState(false)
+ const[form, setForm] = useState({
+  title: " ",
+  video: null,
+  thumbnail: null,
+  prompt: " "
+ })
+
   return (
-    <View>
-      <Text>create</Text>
-    </View>
+   <SafeAreaView className="bg-primary h-full">
+    <ScrollView className="px-4 my-6">
+      <Text className="text-2xl text-white font-psemibold">
+        Upload Video
+      </Text>
+     <FormField
+      title="Video Title"
+      value={form.title}
+      placeholder="Give your video a catch title..."
+      handleChangeText={(e) => setForm({...form, title: e})}
+      otherStyles="mt-10"
+     />
+
+     <View className="mt-7 space-y-2">
+      <Text className="text-base text-gray-100 font-pmedium">
+        Upload Video
+      </Text>
+     </View>
+    </ScrollView>
+   </SafeAreaView>
   )
 }
 
